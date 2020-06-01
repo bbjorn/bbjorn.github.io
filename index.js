2f4446ce85bcf27a5fc3bcf2a0ad2446c2197754
bbjorn <bbjorn@student.chalmers.se>
2020-06-01 15:15:08 +0200

    Plus button
    Added input for plus button

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
-let calcDisplay = 0; 
+let calcDisplay = '0'; 
 
 /** Takes an number input from a button press */ 
 function calcInput(number) {
-    if (calcDisplay == 0 ) calcDisplay = number;
+    if (calcDisplay == '0' ) calcDisplay = number;
     else calcDisplay += '' + number;
     document.getElementById("calcOutput").innerHTML = calcDisplay;
-  }
+}
+
+function calcAdd(){
+    if (calcDisplay == '0' );
+    else if (calcDisplay.charAt(calcDisplay.length-1)=='+');
+    else {
+        calcDisplay += '+';
+        document.getElementById("calcOutput").innerHTML = calcDisplay;
+    }
+}
+
+function calcEquals(){
+
+}
