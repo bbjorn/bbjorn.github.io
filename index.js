4a8d9e8986c728aa47a1977001d6bd5c8a746dc7
bbjorn <bbjorn@student.chalmers.se>
2020-06-01 18:44:34 +0200

    Fixed Bugs
    Made sure that input was chars and fixed bug where multiple decimal points could occour.

--- index.js
+++ index.js
@@ -2,20 +2,42 @@
 let displayMemory = ''; 
 let displayInput = ''; 
 let displayOperator = '';
+let isInteger = true; 
 
-/** Takes an number input from a button press */ 
+/**
+ * Adds a number to the display.
+ * @param {Input number} number 
+ */
 function calcInput(number) {
     if (displayOperator=='') displayMemory = ''; //If last operation was an equals then overwrite it
     if (displayInput == '0') displayInput = number;
     else if (displayInput == ''){
         displayInput = number;
     }
-    else displayInput += '' + number;
-    document.getElementById("calcOutput").innerHTML = displayMemory + 'm' + displayOperator + displayInput;
+    else displayInput +=  number;
+    document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
 }
 
-/** Sets the operator to addition and in case there already is an active calculation it preforms it */
-function calcOperation(operator){
+/** Adds a decimal point */
+function calcDecimal(){
+    if (isInteger){ //Make sure we cannot add more than one decimal point
+        if (displayOperator=='') displayMemory = ''; //If last operation was an equals then overwrite it
+        if (displayInput == ''){
+            displayInput = '0.';
+        }
+        else {
+            displayInput += '.';
+        }
+        isInteger = false; 
+        document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
+    }
+}
+/**
+ * Selects what type of operation is going to be preformed. 
+ * @param {The type of operator that is going to be preformed} operator 
+ */
+ function calcOperation(operator){
+    
     if (displayMemory != '' && displayOperator == '' && displayInput == ''){    //Last operation was equals
         displayOperator = operator;                                                 //Set operator to + 
     } else if (displayOperator == ''){                                          //No operation exists yet
@@ -29,33 +51,40 @@ function calcOperation(operator){
         displayOperator = operator;                                                 //Set operator to +
         displayInput = '';                                                          //Empty input.
     }
+    isInteger = true;
+    document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
     
-    document.getElementById("calcOutput").innerHTML = displayMemory + 'm' + displayOperator + displayInput;
 }
 
 /** Calculates current active caluation */
 function calcEquals(){
-    if(displayMemory != '' && displayInput !='' && displayOperator != ''){     //If every slot has input
-        displayMemory= calcDoMath(displayMemory, displayInput, displayOperator);   //Memory = math;
-        displayOperator = '';                                                      //Clear operator and Input;
-        displayInput = '';    
-        document.getElementById("calcOutput").innerHTML = displayMemory + 'm' + displayOperator + displayInput;
-    }
+    if (displayMemory != '' && displayOperator != '' && displayInput != ''){
+        displayMemory = calcDoMath(displayMemory, displayInput, displayOperator);       // Do the math!                                   
+        displayOperator = '';                                                           //Clear operator and Input;
+        displayInput = '';  
+        isInteger = true;   
+        document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
+        
+    }    
 }
 
 
 /** This where the math happens */
 function calcDoMath(a, b, operator){
-    if (a == '') a = 0; 
+    if (a == '') a = '0'; 
     else numbA = parseFloat(a);
-    if (b == '') b = 0;
+    if (b == '') b = '0';
     else numbB = parseFloat(b);
     if (operator == '+'){
         return (numbA + numbB).toString(); 
     } else if (operator == '-'){
         return (numbA - numbB).toString(); 
     } else if (operator == '*'){
+        if (numbB == 0) return '0'; 
         return (numbA * numbB).toString(); 
+    } else if (operator == '÷'){
+        if (numbB == 0) return null; 
+        return (numbA / numbB).toString(); 
     }
-    return NaN;
+    return null;
 }
