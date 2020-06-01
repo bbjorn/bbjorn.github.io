ff6be6086b36afdbe8e9583052eaf05294178936
bbjorn <bbjorn@student.chalmers.se>
2020-06-01 17:20:54 +0200

    Update index.js
    Added new logic to equals and fixed some bugs.

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ let displayOperator = '';
 
 /** Takes an number input from a button press */ 
 function calcInput(number) {
+    if (displayOperator=='') displayMemory = ''; //If last operation was an equals then overwrite it
     if (displayInput == '0') displayInput = number;
     else if (displayInput == ''){
         displayInput = number;
     }
     else displayInput += '' + number;
-    document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
+    document.getElementById("calcOutput").innerHTML = displayMemory + 'm' + displayOperator + displayInput;
 }
 
 /** Sets the operator to addition and in case there already is an active calculation it preforms it */
 function calcAdd(){
-    if (displayOperator == ''){                                                 //No operation exists yet
+    if (displayMemory != '' && displayOperator == '' && displayInput == ''){    //Last operation was equals
+        displayOperator = '+';                                                      //Set operator to + 
+    } else if (displayOperator == ''){                                          //No operation exists yet
         displayMemory = displayInput;                                               //Move current value to memory
         displayOperator = '+';                                                      //Set operator to + 
         displayInput = '';                                                          //Clear current input  
@@ -27,25 +30,25 @@ function calcAdd(){
         displayInput = '';                                                          //Empty input.
     }
     
-    document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
+    document.getElementById("calcOutput").innerHTML = displayMemory + 'm' + displayOperator + displayInput;
 }
 
 /** Calculates current active caluation */
 function calcEquals(){
-    if (displayMemory.length>0){
-        displayMemory = calcDoMath(displayMemory, displayInput, displayOperator);
-        displayOperator = '';
-        displayInput = '';
-        document.getElementById("calcOutput").innerHTML = displayMemory + displayOperator + displayInput;
-    }   
+    if(displayMemory != '' && displayInput !='' && displayOperator != ''){     //If every slot has input
+        displayMemory= calcDoMath(displayMemory, displayInput, displayOperator);   //Memory = math;
+        displayOperator = '';                                                      //Clear operator and Input;
+        displayInput = '';    
+        document.getElementById("calcOutput").innerHTML = displayMemory + 'm' + displayOperator + displayInput;
+    }
 }
 
 /** This where the math happens */
 function calcDoMath(a, b, operator){
     if (a == '') a = 0; 
-    else numbA = parseInt(a);
+    else numbA = parseFloat(a);
     if (b == '') b = 0;
-    else numbB = parseInt(b);
+    else numbB = parseFloat(b);
     if (operator == '+'){
         return numbA + numbB;; 
     }
