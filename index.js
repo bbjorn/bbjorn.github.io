8ebed9e43085db1fc2084432d04b595d665c6f3d
bbjorn <bbjorn@student.chalmers.se>
2020-06-01 15:47:28 +0200

    Equals functionality
    Equals has now been added. Also Plus does calculations.

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 
+let memory = ''; 
 let calcDisplay = '0'; 
 
 /** Takes an number input from a button press */ 
 function calcInput(number) {
-    if (calcDisplay == '0' ) calcDisplay = number;
+    if (calcDisplay == '0' || calcDisplay == '') calcDisplay = number;
     else calcDisplay += '' + number;
-    document.getElementById("calcOutput").innerHTML = calcDisplay;
+    document.getElementById("calcOutput").innerHTML = memory + calcDisplay;
 }
 
 function calcAdd(){
     if (calcDisplay == '0' );
-    else if (calcDisplay.charAt(calcDisplay.length-1)=='+');
     else {
-        calcDisplay += '+';
-        document.getElementById("calcOutput").innerHTML = calcDisplay;
+        if (memory == '') memory = calcDisplay + '+';
+        else memory = calcDoMath(memory, calcDisplay, memory.charAt(memory.length-1)) + '+';
+        calcDisplay = 0;
+        document.getElementById("calcOutput").innerHTML = memory + calcDisplay;
     }
 }
 
 function calcEquals(){
+    if (memory.length>0){
+        calcDisplay = calcDoMath(memory, calcDisplay, memory.charAt(memory.length-1));
+        memory = '';
+        document.getElementById("calcOutput").innerHTML = memory + calcDisplay;
+    }   
+}
 
+function calcDoMath(a, b, operator){
+    numbA = parseInt(a);
+    numbB = parseInt(b);
+    if (operator == '+'){
+        return numbA + numbB;; 
+    }
 }
